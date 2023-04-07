cae761d15c78569ae0e02dc8d9b317413711cbeb
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-04-07 11:09:01 -0300

    :books: docs: Modificações

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,9 +1,29 @@
+import '../css/Home.css'
+
 function Home() {
     return (
-        <section>
-            <h1>Lista de Compras</h1>
-            <div>
-                
+        <section className="main">
+            <div className='container'>
+                <h1>Lista de Compras</h1>
+                <div className='lista'>
+                   <p>Lista de compras vazia.</p>
+                </div>
+
+                <div className='formulario'>
+                    <form>
+                        <label>Item: </label>
+                        <div>
+                            <input type="text" />
+                        </div>
+                        <label>Quantidade: </label>
+                        <div>
+                            <input type="number" />
+                        </div>
+                        <div className='botao'>
+                            <input id="submit" type="submit" value="Confirmar"/>
+                        </div>
+                    </form>
+                </div>
             </div>
         </section>
     )
