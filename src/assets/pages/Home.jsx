1a10afd55067d60c599c0330b7568b44aa3ed5d7
Thiago Wender Gonsales <thiagowenderg@gmail.com>
2023-04-09 15:02:07 -0300

    :books: docs: Modificações

--- src/assets/pages/Home.jsx
+++ src/assets/pages/Home.jsx
@@ -1,6 +1,8 @@
 import '../css/Home.css'
+import { processamento } from '../components/Lista'
 
 function Home() {
+
     return (
         <section className="main">
             <div className='container'>
@@ -8,16 +10,16 @@ function Home() {
                 <div className='lista' id='lista'>
                    <p>Lista de compras vazia.</p>
                 </div>
-
+                { processamento }
                 <div className='formulario'>
-                    <form>
+                    <form id="form">
                         <label>Item: </label>
                         <div>
-                            <input type="text" />
+                            <input type="text" id="item"/>
                         </div>
                         <label>Quantidade: </label>
                         <div>
-                            <input type="number" />
+                            <input type="number" id='quantidade'/>
                         </div>
                         <div className='botao'>
                             <input id="submit" type="submit" value="Confirmar"/>
